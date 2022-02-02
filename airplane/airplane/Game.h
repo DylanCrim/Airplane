26fe2311cdeb537284154c26acc0286f5789529a
(Student) - Dylan Crimmings <C00273675@itcarlow.ie>
2022-02-02 16:42:51 +0000

    First commit, sky and still plane added

---
+++ airplane/airplane/Game.h
@@ -0,0 +1,49 @@
+/// <summary>
+/// author Pete Lowe May 2019
+/// you need to change the above line or lose marks
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+const int SCREEN_WIDTH = 800u;
+const int SCREEN_HEIGHT = 600u;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::Text m_welcomeMessage; // text used for message on screen
+	sf::Texture m_airplaneTex;
+	sf::Sprite m_airplaneSprite;
+	sf::Texture m_skyTex;
+	sf::Sprite m_skySprite;
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
